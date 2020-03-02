92fda2c6855d6b6626ceb7a0d7973655a2f63f85
zhongxiang <745402208@qq.com>
2020-03-02 19:07:38 +0800

    完善pom文件

--- zuul/src/main/java/cn/zx/biri/zuul/config/SpringMVCConfig.java
+++ zuul/src/main/java/cn/zx/biri/zuul/config/SpringMVCConfig.java
@@ -10,8 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SpringMVCConfig implements WebMvcConfigurer{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/bookImg/**").addResourceLocations("file:E:/gitRep/BiriPic/bookPic/");
-        registry.addResourceHandler("/uploadImg/**").addResourceLocations("file:/");
+        String system = System.getProperty("os.name").toLowerCase();
+        if (system.contains("win")){
+            registry.addResourceHandler("/bookImg/**").addResourceLocations("file:E:/gitRep/BiriPic/bookPic/");
+
+        }else if (system.contains("linux")){
+            registry.addResourceHandler("/bookImg/**").addResourceLocations("file:opt/BiriPic/bookPic/");
+
+        }
+           registry.addResourceHandler("/uploadImg/**").addResourceLocations("file:/");
     }
 
 }
